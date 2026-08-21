2b6a6c996c16efda52804497429e1252b80083cc
SHAMKIREC <konfeta.tvix@gmail.com>
2026-08-21 22:53:30 +0400

    feat: load unified category icon set

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 import '@/styles/final-product-polish.css'
+import '@/styles/category-icons.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
